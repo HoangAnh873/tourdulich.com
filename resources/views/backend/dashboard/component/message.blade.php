e03b61632e91d6a678f1018f749f70f89a2dd863
HoangAnh873 <anhb2103417@gmail.com>
2024-11-12 13:00:46 +0700

    lan32 hoan thien

---
+++ resources/views/backend/dashboard/component/message.blade.php
@@ -0,0 +1,42 @@
+<div class="col-lg-6">
+    <div class="ibox float-e-margins">
+        <div class="ibox-title">
+            <h5>Tin nhắn chờ</h5>
+            <div class="ibox-tools">
+                <a class="collapse-link">
+                    <i class="fa fa-chevron-up"></i>
+                </a>
+                <a class="close-link">
+                    <i class="fa fa-times"></i>
+                </a>
+            </div>
+        </div>
+        <div class="ibox-content ibox-heading">
+            <h3><i class="fa fa-envelope-o"></i>Tin nhắn mới</h3>
+            <small><i class="fa fa-tim"></i> Bạn có 2 thông báo tin nhắn</small>
+        </div>
+        <div class="ibox-content">
+            <div class="feed-activity-list">
+
+                <div class="feed-element">
+                    <div>
+                        <small class="pull-right">5 ngày trước</small>
+                        <strong>Hoàng Anh</strong>
+                        <div>Sao tour duyệt lâu thế ?</div>
+                        <small class="text-muted">8:48 pm - 11.11.2024</small>
+                    </div>
+                </div>
+
+                <div class="feed-element">
+                    <div>
+                        <small class="pull-right">6 ngày trước</small>
+                        <strong>Mỹ Tiên</strong>
+                        <div>Không đặt tour được, cần hỗ trợ.</div>
+                        <small class="text-muted">10:52 am - 08.11.2024</small>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
